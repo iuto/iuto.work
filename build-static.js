27764cb668899a82dc155b9b135cf806a25ae16c
iuto <baiyinyiteng555@gmail.com>
2026-06-20 21:39:18 +0900

    Fix Vercel static deployment

---
+++ build-static.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const outDir = path.join(root, "dist");
+const files = ["index.html", "photos.html", "style.css", "likes-config.js"];
+const folders = ["photos"];
+
+fs.rmSync(outDir, { recursive: true, force: true });
+fs.mkdirSync(outDir, { recursive: true });
+
+for (const file of files) {
+  fs.copyFileSync(path.join(root, file), path.join(outDir, file));
+}
+
+for (const folder of folders) {
+  fs.cpSync(path.join(root, folder), path.join(outDir, folder), { recursive: true });
+}
