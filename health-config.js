4cb1f4e970ce299a8d60dddc090b4004b7756691
iuto <baiyinyiteng555@gmail.com>
2026-06-21 13:59:07 +0900

    🔌 Connect health dashboard to Supabase

--- health-config.js
+++ health-config.js
@@ -1,5 +1,5 @@
 window.IUTO_HEALTH_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://vifnfexxzvvqvfpdqaje.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpZm5mZXh4enZ2cXZmcGRxYWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMDY3MTQsImV4cCI6MjA5NzU4MjcxNH0.TDoSETnMfKcUc6C-Et4sH45EEx5fsUGFOLgIFXc95lI",
   tableName: "health_daily"
 };
