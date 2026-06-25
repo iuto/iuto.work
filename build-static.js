5c592074670d8100f54ae01b53813c606eb3bdaf
iuto <baiyinyiteng555@gmail.com>
2026-06-25 22:33:30 +0900

    ⚡ Optimize photo loading

--- build-static.js
+++ build-static.js
@@ -4,7 +4,8 @@ const path = require("path");
 const root = __dirname;
 const outDir = path.join(root, "dist");
 const files = ["index.html", "photos.html", "health.html", "style.css", "likes-config.js", "health-config.js", "robots.txt"];
-const folders = ["photos"];
+const photoFiles = ["icon.png"];
+const photoFolders = ["thumbs", "large"];
 
 fs.rmSync(outDir, { recursive: true, force: true });
 fs.mkdirSync(outDir, { recursive: true });
@@ -13,6 +14,13 @@ for (const file of files) {
   fs.copyFileSync(path.join(root, file), path.join(outDir, file));
 }
 
-for (const folder of folders) {
-  fs.cpSync(path.join(root, folder), path.join(outDir, folder), { recursive: true });
+const photoOutDir = path.join(outDir, "photos");
+fs.mkdirSync(photoOutDir, { recursive: true });
+
+for (const file of photoFiles) {
+  fs.copyFileSync(path.join(root, "photos", file), path.join(photoOutDir, file));
+}
+
+for (const folder of photoFolders) {
+  fs.cpSync(path.join(root, "photos", folder), path.join(photoOutDir, folder), { recursive: true });
 }
